65528147667d7946c7a59e7cc9cf0c09a24e352e
Tejaswini Khairnar <117677072+Tejaswini1397@users.noreply.github.com>
2023-12-05 18:58:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+document.addEventListener('DOMContentLoaded', function () {
+    const panels = document.querySelectorAll('.panel');
+
+    panels.forEach((panel, index) => {
+        panel.addEventListener('click', function () {
+            // Remove 'active' class from all panels
+            panels.forEach(p => p.classList.remove('active'));
+
+            // Add 'active' class to the clicked panel
+            panel.classList.add('active');
+        });
+    });
+});
